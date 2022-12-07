4202b962ddce3ce4194406f62374089f29ef8bef
Maro de Melo <65258866+immerfallen@users.noreply.github.com>
2022-12-07 22:24:11 +0000

    Inserido método para remover nodo

--- .ccls-cache/@home@runner@EstruturasDeDadosEmC-Arvores/main.c
+++ .ccls-cache/@home@runner@EstruturasDeDadosEmC-Arvores/main.c
@@ -45,15 +45,78 @@ void add(Nodo *n, int value ){
 	}	
 }
 
+void remover(Nodo *n, int value){
+
+	Nodo *filho = n;
+	Nodo *pai;
+	do{
+		pai=filho;
+		if(value < filho->value){
+			filho = filho->esq;
+		} else if(value> filho->value){
+			filho = filho->dir;
+		}
+	}while(filho !=NULL && filho->value != value);
+
+	if(filho != NULL){
+		if(filho->esq == NULL && filho->dir == NULL){ //CASO DO NODO NÃO POSSUIR FILHOS
+			if(pai->esq == filho){
+				pai->esq = NULL;
+			}
+			if(pai->dir == filho){
+				pai->dir = NULL;
+			}
+		}
+		if(filho->esq != NULL && filho->dir == NULL){ //CASO DO NODO FILHO TEM UM FILHO A ESQUERDA
+			if(pai->esq == filho){
+				pai->esq = filho->esq;
+			}
+			if(pai->dir == filho){
+				pai->dir = filho->dir;
+			}
+		}
+
+		if(filho->esq == NULL && filho->dir != NULL){ //CASO DO NODO FILHO TEM UM FILHO A DIREITA
+			if(pai->esq == filho){
+				pai->esq = filho->dir;
+			}
+			if(pai->dir == filho){
+				pai->dir = filho->dir;
+			}
+		}
+
+		if(filho->esq != NULL && filho->dir != NULL){ //CASO DO NODO FILHO TEM DOIS FILHOS
+			if(filho->esq->dir ==NULL){
+				filho->value = filho->esq->value;
+			}
+			else{
+				Nodo *p = filho->esq;
+				Nodo *aux = p;
+				while(p->dir != NULL){
+					aux = p;
+					p=p->dir;
+				}
+				aux->dir = NULL;
+				filho->value = p->value;
+			}
+		}
+		
+	}
+	
+}
+
 int main() {
 
 	Nodo *root = create(5);
 
 	add(root, 2);
 	add(root, 1);
+	add(root, 3);
 	add(root, 8);
 	add(root, 4);
+
 	
+	remover(root, 5);
 
 	imprimir(root);
 	
